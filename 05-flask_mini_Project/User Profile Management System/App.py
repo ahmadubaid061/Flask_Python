4ccf38b14c60c7c87604522139949b24317e4c5c
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-04 14:50:13 +0500

    complete functionality

--- 05-flask_mini_Project/User Profile Management System/App.py
+++ 05-flask_mini_Project/User Profile Management System/App.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,request,Response,session,redirect,url_for
 
 app=Flask(__name__)
+app.secret_key="my_secret_key"
 
 users={
     'admin': {
@@ -12,10 +13,6 @@ users={
          }
 }
 
-# for user,userdata in users.items():
-#     print(f"-------{user}----")
-#     for key, values in userdata.items():
-#         print(f"{key} : {values}")
    
 @app.route("/")
 def home():
@@ -29,16 +26,17 @@ def login():
         # check credentials against DB
         if username in users and users[username]['password'] == password:
           session['username'] = username
-          return redirect(url_for('user_profile.html'))
+          return redirect(url_for('user'))
         else:
             return "Invalid credentials"
-    return render_template('login.html')
+    return render_template('sign_in.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
+        email=request.form.get("email")
         phone=request.form.get('phone')
         hobbies=request.form.getlist('hobbies')
         age=request.form.get('age')
@@ -47,29 +45,32 @@ def register():
         users[username] = {
             'username': username,
             'password': password,
+            'email':email,
             'phone': phone,
             'hobbies': hobbies,
             'age': age
         }
         return redirect(url_for('login'))
-    return render_template('register.html')
+    return render_template('sign_up.html')
 
 
-@app.route('/user_profile')
-def profile():
+@app.route('/user')
+def user():
     if 'username' not in session:
         return redirect(url_for('login'))
     
     username = session['username']
     user = users.get(username)
     
-    return render_template('profile.html', user=user)
+    return render_template('user_profile.html', user=user)
 
 
 @app.route('/logout')
 def logout():
     session.pop("username",None)
-    return redirect(url_for("login.html"))
+    return redirect(url_for("login"))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
