23e32e80629c9cf67977c0f03fbf8b0296c7326c
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-05 15:46:26 +0500

    completed flash messages implementation

--- 06-understanding-flash-messages/App.py
+++ 06-understanding-flash-messages/App.py
@@ -8,9 +8,16 @@ app.secret_key="my_secret_key"
 def form():
     if request.method=="POST":
         name=request.form.get("name")
+        if not name:
+            flash("Name cannot be empty")
+            return redirect(url_for('form'))
+        flash(f"name was saved : '{name}'!")
         email=request.form.get("email")
+        if not email:
+            flash("Email cannot be empty!")
+            return redirect(url_for("form"))
+        flash(f"Email was saved : '{email}'!")
         message=request.form.get("message")
-        
         return render_template("welcome.html",name=name,email=email,message=message)
     return render_template("form.html")
 
