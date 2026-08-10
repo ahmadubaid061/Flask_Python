092a82d390fefd8768532bae34d23c4b7926a7db
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-10 15:27:15 +0500

    added .env for security

--- 10-to-do-App-with-SQLAlchemy/Flask-To-Do-App_SQLALchemy/app/__init__.py
+++ 10-to-do-App-with-SQLAlchemy/Flask-To-Do-App_SQLALchemy/app/__init__.py
@@ -1,13 +1,20 @@
-#here we will create the app and database conection with the app
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
 
-db=SQLAlchemy()
+# load .env into environment variables (do this once, at import time)
+load_dotenv()
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-    app.config['secret_key']
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     db.init_app(app)
+
     return app
