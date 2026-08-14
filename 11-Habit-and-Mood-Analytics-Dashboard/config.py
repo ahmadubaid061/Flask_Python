489eea5d1ce87a30eb41a414dabbb795260c3e0e
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-14 14:47:16 +0500

    v2

--- 11-Habit-and-Mood-Analytics-Dashboard/config.py
+++ 11-Habit-and-Mood-Analytics-Dashboard/config.py
@@ -7,3 +7,10 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') == 'True'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_USERNAME')
