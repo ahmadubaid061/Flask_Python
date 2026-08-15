dd2e90610446d8f7bf9cdec7aa7d17f967b8a77d
ahmadubaid061 <ahmadubaidedu@gmail.com>
2026-08-15 14:23:35 +0500

    vercel deployement configurations

--- 11-Habit-and-Mood-Analytics-Dashboard/wsgi.py
+++ 11-Habit-and-Mood-Analytics-Dashboard/wsgi.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# CRITICAL FIX: Add the current directory to Python's system path
+# This tells Vercel exactly where to find the 'app' folder
+sys.path.insert(0, os.path.dirname(__file__))
+
 from app import create_app
 
 app = create_app()
